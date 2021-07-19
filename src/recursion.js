320fa9e37f33884e5e6153579b39eb951718188f
Charles Ewing <charlesewing3@gmail.com>
2021-07-18 20:44:48 -0400

    Implemented palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -228,6 +228,33 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  // ignore capitals by making all lowercase
+  // get rid of spaces
+  // if string length is 1
+    // return true
+  // if string length is 2
+    // if first char equals second char
+      // return true
+    // else
+      // return false
+  // else
+    // return whether first char equals last char AND palindrome(string.slice(1, string.length-1) is true)
+
+  string = string.toLowerCase();
+  string = string.split(' ').join();
+
+  if (string.length === 1) {
+    return true;
+  }
+
+  if (string.length === 2) {
+    if (string[0] === string[1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return (string[0] === string[string.length-1] && palindrome(string.slice(1, string.length-1)));
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
